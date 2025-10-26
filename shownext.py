41d031c9f7262908d8a02a3a8d0a193f3d3ab41d
Hybridized Help Online <tycha4630@gmail.com>
2025-10-26 02:22:44 +0000

    CSS changes

--- shownext.py
+++ shownext.py
@@ -3,7 +3,7 @@ if selection !=-1:
 questionNum += 1
 
 if questionNum >= len(qadata):
-  updateQuestion("ur done! Your selections:"+ str(selected))
+  updateQuestion("You're done! Your selections:"+ str(selected))
 else:
   question = qadata[questionNum][0]
   answers = qadata[questionNum][1:]
