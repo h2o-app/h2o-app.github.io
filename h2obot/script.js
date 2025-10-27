73be3385df48697d5842e7f0d456b3a4aa0a9865
Hybridized Help Online <tycha4630@gmail.com>
2025-10-27 03:33:52 +0000

    loading sequence for bot

--- h2obot/script.js
+++ h2obot/script.js
@@ -14,11 +14,14 @@ async function fetchData(url) {
 var pyodide
 var next
 async function init(){
+  document.getElementById("answers").innerHTML = "Loading Pyodide..."
   pyodide = await loadPyodide();
+  document.getElementById("answers").innerHTML = "Fetching Data..."
   let AnalyzeR = await fetchData("/AnalyzeR.py")
   let qadata = await fetchData("/qadata.json")
   let start = await fetchData("init.py")
   shownext = await fetchData("shownext.py")
+  document.getElementById("answers").innerHTML = "Loading Content..."
   pyodide.FS.writeFile("/home/pyodide/AnalyzeR.py", AnalyzeR)
   pyodide.FS.writeFile("/qadata.json", qadata)
   pyodide.runPython("import AnalyzeR")
