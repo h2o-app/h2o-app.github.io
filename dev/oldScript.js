c78d800f6c5bb0ed850cf6ce3381658547408c01
Multiplex64 <tycha4630+64@gmail.com>
2026-01-04 14:44:51 -0800

    H2OBot v4.0.0 Convert code to JS

--- dev/oldScript.js
+++ dev/oldScript.js
@@ -68,37 +68,82 @@ function addAnswer(num) {
 
 // Initiate on page load
 async function init() {
+    useNN = false
     questionID = "START"
+    // Wait for pyodide to load
+    async function waitPyodide() {
+        await new Promise(async (resolve) => {
+            fetchedNum--
+            pyodide = await loadPyodide();
+            devInfo.innerHTML += "<br>Pyodide OK!"
+            fetchedNum++
+            resolve()
+        });
+    }
+    // Wait for fetched resource
+    async function waitFetch(path, variable, name) {
+        await new Promise(async (resolve) => {
+            fetchedNum--
+            window[variable] = await fetchData(path);
+            devInfo.innerHTML += "<br>" + name + " OK!"
+            fetchedNum++
+            resolve()
+        });
+    }
+    fetchedNum = 0
     devInfo = document.getElementById("devInfo")
-    devInfo.innerHTML += " OK!<br>"
-    await delay(250)
-    devInfo.innerHTML += "Fetching JSON Data... "
-    data = JSON.parse( await fetchData("data.json"))
-    devInfo.innerHTML += " OK!<br>"
-    await delay(250)
-    devInfo.innerHTML += "<b>Network Requests Complete. No More Internet Connection Required.</b>"
-    await delay(250)
+    devInfo.innerHTML += "OK<br><b>Fetching Resources</b>"
+    if (useNN) {
+        waitPyodide()
+        waitFetch("main.py", "start", "Python Script")
+    }
+    waitFetch("data.json", "qadata", "JSON File")
+    if (useNN) { waitFetch("../AnalyzeR.py", "AnalyzeR", "Neural Network Script") }
+    await delay(500)
+    // Wait for all resources to fetch
+    async function wait() {
+        await new Promise(async (resolve) => {
+
+            while (fetchedNum !== 0) {
+                await delay(100)
+            }
+            resolve();
+        })
+    }
+    await wait()
+    await delay(100)
+    data = JSON.parse(qadata)
+    devInfo.innerHTML += "<br><b>Network Requests Complete. No More Internet Connection Required.</b><br>"
+
+    if (useNN) {
+        devInfo.innerHTML += "Writing JSON Data File... "
+        pyodide.FS.writeFile("/data.json", qadata)
+        devInfo.innerHTML += "<br>Writing Neural Network File... "
+        pyodide.FS.writeFile("/home/pyodide/AnalyzeR.py", AnalyzeR)
+        devInfo.innerHTML += "<br>Running Python Script... "
+        pyodide.runPython(start)
+    }
     devInfo.innerHTML += (`
         <br><b>Starting H2OBot...</b><br><br>
         <button class="link cn"onclick="document.getElementById('devInfo').innerHTML = ''">
         <b>Hide Initiation Log</b></button><hr><br>
     `)
-    await delay(250)
+    await delay(500)
     document.getElementById("primary").innerHTML += "This is the start of your H2OBot History.<br>"
-    await delay(250)
+    await delay(500)
     addQuestion()
-    await delay(250)
+    await delay(500)
     addOptions()
 }
 
 // Run by button press, to add answer to question and then 
 async function nextQuestion(num, value) {
     addResponse(value)
-    await delay(250)
+    await delay(500)
     addAnswer(num)
-    await delay(250)
+    await delay(500)
     addQuestion()
-    await delay(250)
+    await delay(500)
     addOptions()
 }
 
