f3ef408d563a5528828cb7037768b454c4fd8a56
Multiplex64 <tycha4630+64@gmail.com>
2026-01-04 21:15:26 -0800

    H2OBot v4.1.0 timing changes, offline version

--- h2obot/script.js
+++ h2obot/script.js
@@ -1,3 +1,7 @@
+primary = document.getElementById("primary")
+secondary = document.getElementById("secondary")
+devInfo = document.getElementById("devInfo")
+
 async function fetchData(url) {
     try {
         const response = await fetch(url);
@@ -68,40 +72,77 @@ function addAnswer(num) {
 
 // Initiate on page load
 async function init() {
+    offlineMode = false;
     questionID = "START"
-    devInfo = document.getElementById("devInfo")
     devInfo.innerHTML += " OK!<br>"
-    await delay(250)
-    devInfo.innerHTML += "Fetching JSON Data... "
-    data = JSON.parse( await fetchData("data.json"))
-    devInfo.innerHTML += " OK!<br>"
-    await delay(250)
-    devInfo.innerHTML += "<b>Network Requests Complete. No More Internet Connection Required.</b>"
-    await delay(250)
+    await delay(100)
+    if (offlineMode) {
+        rawdata = "REPLACE"
+        data = JSON.parse(rawdata)
+        devInfo.innerHTML += "<b>Viewing in Offline Mode. No Internet Connection Required.</b>"
+    } else {
+        devInfo.innerHTML += "Fetching JSON Data... "
+        data = JSON.parse(await fetchData("data.json"))
+        devInfo.innerHTML += " OK!<br>"
+        await delay(100)
+        devInfo.innerHTML += "<b>Network Requests Complete. No More Internet Connection Required.</b>"
+    }
+    await delay(100)
     devInfo.innerHTML += (`
-        <br><b>Starting H2OBot...</b><br><br>
+        <br><b>Starting H2OBot...</b>
+        <br><br>
         <button class="link cn"onclick="document.getElementById('devInfo').innerHTML = ''">
-        <b>Hide Initiation Log</b></button><hr><br>
+        <b>Hide Initiation Log</b></button><br>
+        <button class="link cn"onclick="downloadPage()">
+        <b>Download Offline Version</b></button>
+        <hr><br>
     `)
-    await delay(250)
-    document.getElementById("primary").innerHTML += "This is the start of your H2OBot History.<br>"
-    await delay(250)
+    // Start the actual bot
+    await delay(200)
+    primary.innerHTML += "This is the start of your H2OBot History.<br>"
+    await delay(200)
     addQuestion()
-    await delay(250)
+    await delay(200)
     addOptions()
 }
 
 // Run by button press, to add answer to question and then 
 async function nextQuestion(num, value) {
     addResponse(value)
-    await delay(250)
+    await delay(200)
     addAnswer(num)
-    await delay(250)
+    await delay(200)
     addQuestion()
-    await delay(250)
+    await delay(200)
     addOptions()
 }
 
+async function downloadPage() {
+    html = await fetchData("index.html")
+    css = await fetchData("../style.css")
+    js = await fetchData("script.js")
+    json = await fetchData("data.json")
+
+    // Insert CSS into HTML
+    cssReplaced = html.replace('<link href="../style.css" rel="stylesheet" type="text/css" />', "<style>" + css + "</style>")
+
+    // change JS for offline mode
+    offlineMode = js.replace('offlineMode = false', "offlineMode = true")
+
+    // Insert JSON into JS
+    jsonReplaced = offlineMode.replace('rawdata = "REPLACE"', "rawdata = `" + json + "`")
+
+    // Combine HTML and JS changes
+    html_json = cssReplaced.replace('<script src="script.js"></scr' + 'ipt>', "<script>" + jsonReplaced + '</scr' + 'ipt>')
+
+    blob = new Blob([html_json], { type: 'text/plain' })
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'H2OBot.html'
+    document.body.appendChild(a)
+    a.click()
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     init();
